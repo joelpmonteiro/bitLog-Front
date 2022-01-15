a8e825d4a36c3086a80dfdb2f81f6d317833c56f
joelpmonteiro50 <joelpmonteiro50@gmail.com>
2022-01-15 13:39:14 -0300

    teste
    teste

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3333',
+    baseURL: 'https://bitlog5.herokuapp.com/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
